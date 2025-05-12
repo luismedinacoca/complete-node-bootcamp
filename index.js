3ee5b5cae12ec1d40461e1c7daa927156207053f
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-12 17:17:04 -0300

    Section 02 - Lecture 020: Using Modules 3: 3rd Party Modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url')
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate')
 
 /********************************** FILES **********************************/
@@ -36,24 +36,6 @@ console.log("Will read file!");
 
 
 /********************************** SERVER **********************************/
-
-/*
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/NOT_ORGANIC/g, 'not-organic');
-
-    return output;
-}
-*/
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -61,6 +43,12 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 //console.log("dataObj: ", dataObj);
+
+console.log(slugify('Fresh Avocados', {lowercase: true}));
+
+// create slugs array:
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log("🤪 slugs", slugs);
 
 const server = http.createServer( (req, res) => {
     //console.log(req.url);
@@ -92,7 +80,7 @@ const server = http.createServer( (req, res) => {
         //res.end("This is the PRODUCT page");
         res.end(output);
 
-    // API:    
+    // API:     
     } else if(pathname === '/api'){
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
